d391a3d55b31d3a85e78e5678d6795c98c062407
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2022-02-07 21:21:38 +0100

    impl initial functionality

--- lua/project_rc.lua
+++ lua/project_rc.lua
@@ -1,13 +1,43 @@
+local v = vim
 local M = {}
+local log = require('project_rc.log')
 
-local function with_defaults(options)
-   return {
-      name = options.name or "example"
-   }
+local function defaultConfig()
+    return {
+        config_file_name = ".nvimrc.lua",
+        projects = { -- The projects for which to source a local config file
+            -- "/home/user/repos/my_project"
+        } 
+    }
+end
+
+function M.onDirChanged()
+    log.info("DirChanged, checking whether to load local config file")
+    local curDir = vim.fn.getcwd()
+
+    for _, projectPattern in ipairs(M.options.projects) do
+        -- TODO allow pattern?
+        if curDir == projectPattern then
+            local localConfigFile = curDir .. "/" .. M.options.config_file_name
+            if v.fn.filereadable(localConfigFile) then
+                log.info("Sourcing local config file: " .. localConfigFile)
+                v.cmd("source "..localConfigFile)
+            end
+            -- TODO in case of directory, add it to rtp?
+        end
+    end
 end
 
 function M.setup(options)
-   M.options = with_defaults(options)
+    M.options = v.tbl_extend("keep", options, defaultConfig())
+
+    v.cmd([[
+        " augroup project_rc
+            " autocmd!
+            au DirChanged * lua require('project_rc').onDirChanged()
+        " augroup END
+    ]])
+    M.onDirChanged() -- Call once for initial start cwd, which is not transmitted by autocmd
 end
 
 return M
